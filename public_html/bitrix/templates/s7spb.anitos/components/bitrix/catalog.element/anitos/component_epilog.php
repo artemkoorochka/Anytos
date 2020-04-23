cebcc26040d73963fc133532346215a584e4ea95
artemkoorochka <artem@koorochka.com>
2020-04-23 21:18:16 +0300

    Send out data from cache. Cach this out on epilog

---
+++ public_html/bitrix/templates/s7spb.anitos/components/bitrix/catalog.element/anitos/component_epilog.php
@@ -0,0 +1,12 @@
+<?
+/**
+ * @var array $arResult
+ * @var array $arParams
+ */
+if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
+
+
+global $brandInfo;
+$brandInfo = $arResult["H_COMPANY"];
+
+
