85823f6950e2387c38a50da9feb4add1767100c2
artemkoorochka <artem@koorochka.com>
2020-04-24 22:04:45 +0300

    Multilevel layout

--- public_html/bitrix/templates/s7spb.anitos/components/bitrix/menu/s7sbp.catalog.sections/template.php
+++ public_html/bitrix/templates/s7spb.anitos/components/bitrix/menu/s7sbp.catalog.sections/template.php
@@ -8,12 +8,257 @@ if(empty($arResult))
 ?>
 
 
-<div id="s7sbp-catalog-sections">
+<ul id="s7sbp-catalog-sections">
 
     <div class="text-15 text-danger text-bold text-uppercase pb3 px3">Канцтовары</div>
 
-    <?foreach ($arResult as $arItem):?>
-        <a href="/catalog/section/" class="d-block text-roboto py1 px3">Бумажно-беловые товары</a>
+
+    <?foreach ($arResult as $i=>$arItem):?>
+        <li class="nav-item level-1">
+            <a href="/catalog/section/" class="text-roboto level-1">Бумажно-беловые товары</a>
+
+            <ul class="submenu">
+
+                <li class="nav-item level-2">
+                    <a href="/category/1843-hasbro" class="">HASBRO</a>
+
+                </li>
+
+                <li class="nav-item level-2">
+                    <a href="/category/1846-lego" class="">LEGO (Лего)</a>
+
+                </li>
+
+                <li class="nav-item level-2">
+                    <a href="/category/1092-brendovye_igrushki_dlya_detey_do_1_goda" class="">Брендовые игрушки для детей до 1 года</a>
+
+                </li>
+
+                <li class="nav-item level-2">
+                    <a href="/category/423-igrushki_muzykalnye_dlja_detej" class="">Детские музыкальные инструменты</a>
+
+                </li>
+
+                <li class="nav-item level-2">
+                    <a href="/category/1911-zavodnye_igrushki" class="">Заводные игрушки</a>
+
+                </li>
+
+                <li class="nav-item level-2">
+                    <a href="/category/874-igrovye_nabory_dobryy_mir_svinki_peppy_-peppa_pig" class="">Игровые наборы Добрый мир Свинки Пеппы (Peppa Pig)</a>
+
+                </li>
+
+                <li class="nav-item level-2">
+                    <a href="/category/428-igroteka_igrushki_dlya_razvlecheniy_i_zabav" class="">Игротека, игрушки для развлечений и забав</a>
+
+                    <ul>
+
+                        <li class="nav-item level-3"><a href="/category/189-binokli">Бинокли</a></li>
+                        <li class="nav-item level-3"><a href="/category/2237-vertushki">Вертушки</a></li>
+                        <li class="nav-item level-3"><a href="/category/279-vozdushnye_zmei">Воздушные змеи</a></li>
+                        <li class="nav-item level-3"><a href="/category/2238-igrushki_jo-jo">Игрушки Йо-Йо, спирали</a></li>
+                        <li class="nav-item level-3"><a href="/category/2240-kalejdoskopy">Калейдоскопы</a></li>
+                        <li class="nav-item level-3"><a href="/category/1579-labirinty_detskie">Лабиринты детские</a></li>
+                        <li class="nav-item level-3"><a href="/category/2239-letajucshie_tarelki_bumerangi">Летающие тарелки, бумеранги</a></li>
+                        <li class="nav-item level-3"><a href="/category/602-spinnery">Спиннеры</a></li>
+                        <li class="nav-item level-3"><a href="/category/773-shariki_dlja_suhogo_bassejna">Шарики для сухого бассейна</a></li>
+                    </ul>
+
+                </li>
+
+                <li class="nav-item level-2">
+                    <a href="/category/1332-podushki_antistress_i_igrushki_antistress" class="">Игрушки антистресс, сквиши</a>
+
+                    <ul>
+
+                        <li class="nav-item level-3"><a href="/category/602-spinnery">Спиннеры</a></li>
+                    </ul>
+
+                </li>
+
+                <li class="nav-item level-2">
+                    <a href="/category/424-igrushki_dlya_devochek" class="">Игрушки для девочек</a>
+
+                    <ul>
+
+                        <li class="nav-item level-3"><a href="/category/872-igrovye_nabory_dlya_devochek">Игровые наборы для девочек</a></li>
+                        <li class="nav-item level-3"><a href="/category/873-kolyaski-i-mebel-dlya-kukol">Коляски и мебель для кукол</a></li>
+                        <li class="nav-item level-3"><a href="/category/871-kukly_i_pupsy">Куклы и пупсы</a></li>
+                        <li class="nav-item level-3"><a href="/category/1227-kukolnye_domiki">Кукольные домики</a></li>
+                        <li class="nav-item level-3"><a href="/category/2169-kukolnye_teatry">Кукольные театры</a></li>
+                    </ul>
+
+                </li>
+
+                <li class="nav-item level-2">
+                    <a href="/category/429-igrushki_dlya_detey_do_treh_let" class="">Игрушки для детей до трех лет</a>
+
+                    <ul>
+
+                        <li class="nav-item level-3"><a href="/category/1573-katalki_detskie_na_palochke">Каталки детские</a></li>
+                        <li class="nav-item level-3"><a href="/category/2163-kubiki">Кубики</a></li>
+                        <li class="nav-item level-3"><a href="/category/2156-matreshki">Матрешки</a></li>
+                        <li class="nav-item level-3"><a href="/category/2157-nevaljashki">Неваляшки</a></li>
+                        <li class="nav-item level-3"><a href="/category/2153-piramidki">Пирамидки</a></li>
+                        <li class="nav-item level-3"><a href="/category/2186-rybalki">Рыбалки</a></li>
+                        <li class="nav-item level-3"><a href="/category/2154-sortery_vkladyshi">Сортеры, вкладыши</a></li>
+                        <li class="nav-item level-3"><a href="/category/2155-shnurovki">Шнуровки</a></li>
+                        <li class="nav-item level-3"><a href="/category/2158-july">Юлы</a></li>
+                    </ul>
+
+                </li>
+
+                <li class="nav-item level-2">
+                    <a href="/category/1145-igrushki_dlya_kupaniya" class="">Игрушки для купания</a>
+
+                </li>
+
+                <li class="nav-item level-2">
+                    <a href="/category/425-igrushki_dlya_malchikov" class="">Игрушки для мальчиков</a>
+
+                    <ul>
+
+                        <li class="nav-item level-3"><a href="/category/1886-vertolety_i_zapchasti_k_vertoletam">Вертолеты и запчасти к вертолетам</a></li>
+                        <li class="nav-item level-3"><a href="/category/188-vodnyj_transport">Водный транспорт</a></li>
+                        <li class="nav-item level-3"><a href="/category/777-modeli_voennoj_tehniki">Военная техника</a></li>
+                        <li class="nav-item level-3"><a href="/category/882-garaji_treki_jeleznye_dorogi">Гаражи, треки, парковки</a></li>
+                        <li class="nav-item level-3"><a href="/category/879-detskoe_orujie">Детское оружие</a></li>
+                        <li class="nav-item level-3"><a href="/category/2145-zheleznye_dorogi_i_detali_k_nim">Железные дороги и детали к ним</a></li>
+                        <li class="nav-item level-3"><a href="/category/876-igrovye_nabory_dlya_malchikov">Игровые наборы для мальчиков</a></li>
+                        <li class="nav-item level-3"><a href="/category/2397-mashinki-detskie">Машинки детские</a></li>
+                        <li class="nav-item level-3"><a href="/category/2188-roboty_na_radioupravlenii_i_elektrificirovannye">Роботы на радиоуправлении и электрифицированные</a></li>
+                        <li class="nav-item level-3"><a href="/category/796-samolety_i_kosmicheskie_korabli">Самолеты и космические корабли</a></li>
+                        <li class="nav-item level-3"><a href="/category/1875-sbornye_modeli">Сборные модели</a></li>
+                        <li class="nav-item level-3"><a href="/category/2146-upravlenie_dorozhnym_dvizheniem">Управление дорожным движением</a></li>
+                        <li class="nav-item level-3"><a href="/category/436-shpionskie_shtuchki">Шпионские штучки</a></li>
+                    </ul>
+
+                </li>
+
+                <li class="nav-item level-2">
+                    <a href="/category/431-igrushki-dlya-novorozhdennyh" class="">Игрушки для новорожденных</a>
+
+                </li>
+
+                <li class="nav-item level-2">
+                    <a href="/category/434-igrushki_iz_dereva_dlya_detey" class="">Игрушки из дерева для детей</a>
+
+                </li>
+
+                <li class="nav-item level-2">
+                    <a href="/category/426-igrushki_na_batareykah" class="">Игрушки на батарейках</a>
+
+                </li>
+
+                <li class="nav-item level-2">
+                    <a href="/category/1859-igrushki_plastizol" class="">Игрушки Пластизоль</a>
+
+                </li>
+
+                <li class="nav-item level-2">
+                    <a href="/category/1157-igrushki_figurki" class="">Игрушки фигурки</a>
+
+                    <ul>
+
+                        <li class="nav-item level-3"><a href="/category/881-jivotnye_i_dinozavry_-figurki_nabory">Животные и динозавры (фигурки, наборы)</a></li>
+                        <li class="nav-item level-3"><a href="/category/2179-nabory_piratov">Наборы пиратов</a></li>
+                        <li class="nav-item level-3"><a href="/category/2180-nabory_rycarej">Наборы рыцарей</a></li>
+                        <li class="nav-item level-3"><a href="/category/2181-nabory_specsluzhb">Наборы спецслужб</a></li>
+                        <li class="nav-item level-3"><a href="/category/2182-soldatiki_voennye_nabory">Солдатики, военные наборы</a></li>
+                        <li class="nav-item level-3"><a href="/category/877-transformery-_lyubimye_geroi">Трансформеры роботы</a></li>
+                        <li class="nav-item level-3"><a href="/category/2235-figurki_geroi_multfilmov">Фигурки герои мультфильмов</a></li>
+                    </ul>
+
+                </li>
+
+                <li class="nav-item level-2">
+                    <a href="/category/433-interaktivnye_igrushki_dlya_detey" class="">Интерактивные игрушки для детей</a>
+
+                </li>
+
+                <li class="nav-item level-2">
+                    <a href="/category/422-konstruktory" class="">Конструкторы</a>
+
+                    <ul>
+
+                        <li class="nav-item level-3"><a href="/category/1846-lego">LEGO (Лего)</a></li>
+                        <li class="nav-item level-3"><a href="/category/1197-derevyannyj_konstruktor">Деревянный конструктор</a></li>
+                        <li class="nav-item level-3"><a href="/category/1855-konstruktory_mega_bloks">Конструкторы Mega Bloks</a></li>
+                        <li class="nav-item level-3"><a href="/category/1921-konstruktory_dlja_devochek">Конструкторы для девочек</a></li>
+                        <li class="nav-item level-3"><a href="/category/190-konstruktory_dlja_malyshej">Конструкторы для малышей</a></li>
+                        <li class="nav-item level-3"><a href="/category/2150-konstruktory_dlja_malyshej_iz_vyduvnoj_plastmassy">Конструкторы для малышей из выдувной пластмассы</a></li>
+                        <li class="nav-item level-3"><a href="/category/878-konstruktory_dlya_malchikov">Конструкторы для мальчиков</a></li>
+                        <li class="nav-item level-3"><a href="/category/2083-konstruktor_dlja_truda">Конструкторы металлические, кострукторы для труда</a></li>
+                        <li class="nav-item level-3"><a href="/category/1854-konstruktory_lego">Конструкторы типа LEGO</a></li>
+                        <li class="nav-item level-3"><a href="/category/2185-konstruktory_sharnirnye">Конструкторы шарнирные</a></li>
+                        <li class="nav-item level-3"><a href="/category/1311-magnitnyj_konstruktor">Магнитный конструктор</a></li>
+                        <li class="nav-item level-3"><a href="/category/1849-mjagkij_konstruktor">Мягкий конструктор</a></li>
+                        <li class="nav-item level-3"><a href="/category/1848-neobychnye_konstruktory">Необычные конструкторы</a></li>
+                    </ul>
+
+                </li>
+
+                <li class="nav-item level-2">
+                    <a href="/category/1141-korziny_dlja_detskih_igrushek" class="">Корзины для детских игрушек</a>
+
+                </li>
+
+                <li class="nav-item level-2">
+                    <a href="/category/2152-lizuny_zhvachka_dlja_ruk" class="">Лизуны, тягуны, жвачка для рук, слаймы</a>
+
+                </li>
+
+                <li class="nav-item level-2">
+                    <a href="/category/430-malenkaya_radost" class="">Маленькая радость</a>
+
+                </li>
+
+                <li class="nav-item level-2">
+                    <a href="/category/2159-muzykalnye_igrushki" class="">Музыкальные игрушки</a>
+
+                </li>
+
+                <li class="nav-item level-2">
+                    <a href="/category/432-muzykalnye_kovriki" class="">Музыкальные коврики</a>
+
+                </li>
+
+                <li class="nav-item level-2">
+                    <a href="/category/421-myagkie-igrushki" class="">Мягкие игрушки</a>
+
+                    <ul>
+
+                        <li class="nav-item level-3"><a href="/category/2162-krupnye_mjagkie_igrushki_ot_40sm">Крупные мягкие игрушки (от 40см)</a></li>
+                        <li class="nav-item level-3"><a href="/category/2161-melkie_mjagkie_igrushki_do_20sm">Мелкие мягкие игрушки (до 20см)</a></li>
+                        <li class="nav-item level-3"><a href="/category/2160-srednie_mjagkie_igrushki_20-40sm">Средние мягкие игрушки (20-40см)</a></li>
+                    </ul>
+
+                </li>
+
+                <li class="nav-item level-2">
+                    <a href="/category/427-pesochnye_nabory" class="">Песочные наборы</a>
+
+                </li>
+
+                <li class="nav-item level-2">
+                    <a href="/category/1059-ponicikly_kachalki" class="">Понициклы, качалки, каталки, машины-каталки</a>
+
+                </li>
+
+                <li class="nav-item level-2">
+                    <a href="/category/420-razvivayuschie_igry_i_igrushki_dlya_detey" class="">Развивающие игры и игрушки для детей</a>
+
+                </li>
+
+                <li class="nav-item level-2">
+                    <a href="/category/502-razvivayuschie_kovriki_razvivayuschie_centry_" class="">Развивающие коврики, развивающие центры</a>
+
+                </li>
+
+            </ul>
+
+        </li>
     <?endforeach;?>
 
-</div>
+</ul>
