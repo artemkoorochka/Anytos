1af46b3b3346b8c705bf374deb9f267e846f163b
artemkoorochka <artem@koorochka.com>
2020-04-29 11:28:59 +0300

    Layout

---
+++ public_html/bitrix/templates/s7spb.anitos/components/bitrix/menu/s7sbp.nav.catalog/template.php
@@ -0,0 +1,38 @@
+<?
+/**
+ * @var array $arResult
+ * @var array $arParams
+ */
+if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true) die();
+?>
+
+<div id="s7sbp-nav-catalog">
+
+    <div class="s7spb-row">
+        <a href="/catalog/" class="s7spb-col"><i class="icon icon-appliances"></i> Бытовая техника</a>
+        <a href="/catalog/" class="s7spb-col"><i class="icon icon-children-toys"></i>Игрушки для детей</a>
+        <a href="/catalog/" class="s7spb-col"><i class="icon icon-сosmetics"></i>Косметика и парфюмерия</a>
+        <a href="/catalog/" class="s7spb-col"><i class="icon icon-lighting"></i>Освещение</a>
+        <a href="/catalog/" class="s7spb-col"><i class="icon icon-lux"></i>Товары «ЛЮКС»</a>
+        <a href="/catalog/" class="s7spb-col"><i class="icon icon-school"></i>Товары для школы</a>
+    </div>
+
+    <div class="s7spb-row">
+        <a href="/catalog/" class="s7spb-col"><i class="icon icon-haberdashery"></i>Галантерея</a>
+        <a href="/catalog/" class="s7spb-col"><i class="icon icon-stationery"></i>Канцтовары</a>
+        <a href="/catalog/" class="s7spb-col"><i class="icon icon-clothing"></i>Одежда и Текстиль</a>
+        <a href="/catalog/" class="s7spb-col"><i class="icon icon-food"></i>Продукты питания  и посуда</a>
+        <a href="/catalog/" class="s7spb-col"><i class="icon icon-children-goods"></i>Товары для детей</a>
+        <div class="s7spb-col"></div>
+    </div>
+
+    <div class="s7spb-row">
+        <a href="/catalog/" class="s7spb-col"><i class="icon icon-children-shoes"></i>Детская обувь</a>
+        <a href="/catalog/" class="s7spb-col"><i class="icon icon-books"></i>Книги</a>
+        <a href="/catalog/" class="s7spb-col"><i class="icon icon-сelebration"></i>Праздник</a>
+        <a href="/catalog/" class="s7spb-col"><i class="icon icon-souvenirs"></i>Сувениры</a>
+        <a href="/catalog/" class="s7spb-col"><i class="icon icon-household"></i>Товары для дома</a>
+        <div class="s7spb-col"></div>
+    </div>
+
+</div>
