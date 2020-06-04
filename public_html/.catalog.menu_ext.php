9382cbe2b927805da7a4e2bba92cc258bfae721b
artemkoorochka <artem@koorochka.com>
2020-06-04 13:02:22 +0300

    studio7sbp:iblock.sections.menu variant

--- public_html/.catalog.menu_ext.php
+++ public_html/.catalog.menu_ext.php
@@ -1,99 +1,31 @@
 <?
-use Bitrix\Main\Loader;
-
 if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
-if(!Loader::includeModule("iblock"))
-    return;
-
-$arParams = [
-    "IBLOCK_TYPE" => "marketplace",
-    "IBLOCK_ID" => "2",
-    "ACTIVE" => "Y",
-    "GLOBAL_ACTIVE" => "Y",
-    "DEPTH_LEVEL" => 1
-];
-
+global $APPLICATION;
 $aMenuLinksExt = [];
 
-$sections = CIBlockSection::GetList(
-    ["DEPTH_LEVEL" => "ASC"],
-    $arParams,
-    false,
-    ["ID", "NAME", "DEPTH_LEVEL", "UF_PARENT", "SECTION_PAGE_URL", "DEPTH_LEVEL"]
-);
-while ($section = $sections->GetNext())
-{
+$arParams = [
+    "FILTER" => [
+        "IBLOCK_ID" => "2",
+        "ACTIVE" => "Y",
+        "GLOBAL_ACTIVE" => "Y",
+        "<DEPTH_LEVEL" => 3
+    ]
+];
 
-    if(!empty($section["UF_PARENT"])){
-        unset($section["UF_PARENT"][array_search($section["ID"],$section["UF_PARENT"])]);
-    }
+$arResult = $APPLICATION->IncludeComponent("studio7sbp:iblock.sections.menu", null, $arParams, null, ["HIDE_ICONS" => "Y"]);
 
-    $arSections[$section["ID"]] = $section;
-
-}
-
-foreach ($arSections as $section){
-
-    $section["IS_PARENT"] = false;
-    if(!empty($section["UF_PARENT"])){
-        foreach ($section["UF_PARENT"] as $key=>$parentSection){
-
-            if($parentSection == $section["ID"]){
-                unset($arSections[$parentSection["ID"]]["UF_PARENT"][$key]);
-                continue;
-            }
-
-            if($arSections[$parentSection["ID"]]["ID"] > 0 && $arSections[$parentSection["ID"]]["DEPTH_LEVEL"] < $section["DEPTH_LEVEL"]){
-
-                $arSections[$parentSection["ID"]]["NAME"] .= " test name " . $arSections[$parentSection["ID"]]["NAME"] . " test level = " . $arSections[$parentSection["ID"]]["DEPTH_LEVEL"];
-
-                $section["IS_PARENT"] = true;
-                break;
-            }
-        }
-    }
-    $arSections[$section["ID"]]["IS_PARENT"] = $section["IS_PARENT"];
-}
-
-foreach ($arSections as $section)
+foreach ($arResult as $section)
 {
     $aMenuLinksExt[] = [
-        $section["NAME"] . "[" . $section["ID"] . "] - " . implode("#", $section["UF_PARENT"]),
+        $section["NAME"],
         $section["SECTION_PAGE_URL"],
         [],
         [
-            "FROM_IBLOCK" => $arParams["IBLOCK_ID"],
-            "IS_PARENT" => (empty($section["UF_PARENT"]) ? false : true),
+            "FROM_IBLOCK" => $arParams["FILTER"]["IBLOCK_ID"],
+            "IS_PARENT" => (empty($section["CHIELDS"]) ? false : true),
             "DEPTH_LEVEL" => $section["DEPTH_LEVEL"]
         ]
     ];
-
-    addMenuSection($aMenuLinksExt, $arSections, $section, $arParams);
-
-
-}
-
-function addMenuSection(&$aMenuLinksExt, $arSections, $section, $arParams){
-    // UF_PARENT
-    if(!empty($section["UF_PARENT"])){
-        foreach ($section["UF_PARENT"] as $parentSection){
-
-            if(!empty($parentSection["ID"])){
-                $aMenuLinksExt[] = [
-                    $arSections[$parentSection["ID"]]["NAME"],
-                    $arSections[$parentSection["ID"]]["SECTION_PAGE_URL"],
-                    [],
-                    [
-                        "FROM_IBLOCK" => $arParams["IBLOCK_ID"],
-                        "IS_PARENT" =>  (empty($arSections[$parentSection["ID"]]["UF_PARENT"]) ? false : true),
-                        "DEPTH_LEVEL" => $arSections[$parentSection["ID"]]["DEPTH_LEVEL"]
-                    ]
-                ];
-                addMenuSection($aMenuLinksExt, $arSections, $arSections[$parentSection["ID"]], $arParams);
-            }
-
-        }
-    }
 }
 
 $aMenuLinks = array_merge($aMenuLinks, $aMenuLinksExt);
