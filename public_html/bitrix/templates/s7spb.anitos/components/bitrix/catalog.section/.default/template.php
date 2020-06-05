71bafdc959c33b7bf378b7dbb5aac099f940eea3
artemkoorochka <artem@koorochka.com>
2020-06-05 11:34:32 +0300

    loyout version

--- public_html/bitrix/templates/s7spb.anitos/components/bitrix/catalog.section/.default/template.php
+++ public_html/bitrix/templates/s7spb.anitos/components/bitrix/catalog.section/.default/template.php
@@ -14,6 +14,11 @@ if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
 $this->setFrameMode(true);
 if(empty($arResult["ITEMS"]))
     return;
+
+$this->SetViewTarget("CATALOG_SECTION_NAME");
+    echo $arResult["NAME"];
+$this->EndViewTarget();
+
 ?>
 
 <div class="bg-white border-radius-10 py2 px1">
@@ -23,6 +28,9 @@ if(empty($arResult["ITEMS"]))
     <?endif?>
 
     <div class="s7spb-row s7spb-row12">
+
+
+
     <?
     foreach($arResult["ITEMS"] as $key=>$arElement):
         $this->AddEditAction($arElement['ID'], $arElement['EDIT_LINK'], CIBlock::GetArrayByID($arParams["IBLOCK_ID"], "ELEMENT_EDIT"));
@@ -39,8 +47,14 @@ if(empty($arResult["ITEMS"]))
     ?>
 
         <div id="<?=$this->GetEditAreaId($arElement['ID']);?>" class="s7spb-col text-center pb5 position-relative">
+
             <div class="s7spb-product-cell">
-                <a href="#" class="d-block text-decoration-none text-tranqulity text-12 text-capitalize">
+
+                <?if($arElement["PROPERTIES"]["DISCOUNT"]["VALUE"]):?>
+                    <div class="<?=$arElement["STIKER"]["CLASS"]?>" title="<?=$arElement["STIKER"]["TITLE"]?>"><?=$arElement["STIKER"]["CODE"]?></div>
+                <?endif;?>
+
+                <a href="<?=$arElement["DETAIL_PAGE_URL"]?>" class="d-block text-decoration-none text-tranqulity text-12 text-capitalize">
 
                     <?if(is_array($arElement["PREVIEW_PICTURE"])):?>
                         <div class="pb3">
@@ -53,15 +67,70 @@ if(empty($arResult["ITEMS"]))
                         </div>
                     <?endif;?>
 
-                    <div class="pb1"><?=$arElement["NAME"]?></div>
+                    <div class="s7spb-product-name"><?=$arElement["NAME"]?></div>
                 </a>
 
-                <?foreach($arElement["PRICES"] as $code=>$arPrice):?>
-                    <div class="text-<?=$code?> <?=$code == "price_yellow" ? "text-16" : "text-10"?>"
-                         title="<?=$arResult["PRICES"][$code]["TITLE"]?>">
-                        <?=$arPrice["PRINT_VALUE"]?>
+                <?if(empty($arElement["PRICES"])):?>
+                    <?if(is_array($arElement["DISPLAY_PROPERTIES"])):?>
+                        <div class="s7spb-product-props">
+                            <?foreach ($arElement["DISPLAY_PROPERTIES"] as $arProperty):?>
+                                <div class="pb1 text-11"><?=$arProperty["NAME"]?>: <b><?=$arProperty["DISPLAY_VALUE"]?></b></div>
+                            <?endforeach;?>
+                            <div class="pb1 text-11">Остаток: <b>>100</b></div>
+                        </div>
+                    <?endif;?>
+                <?else:?>
+                    <table class="pb2" align="center">
+                        <tr>
+                            <td>
+                                <?foreach($arElement["PRICES"] as $code=>$arPrice):?>
+                                    <div class="text-<?=$code?> <?=$code == "price_yellow" ? "text-16" : "text-10"?>"
+                                         title="<?=$arResult["PRICES"][$code]["TITLE"]?>">
+                                        <?=$arPrice["PRINT_VALUE"]?>
+                                    </div>
+                                <?endforeach;?>
+                            </td>
+                            <td class="s7spb-product-informer">
+                                <div class="circle-24 text-20 ml1 cursor-pointer bg-gray text-bold text-gray d-inline-block"
+                                     onclick="anitosCatalogSection.priceInfoToggle(this)">i</div>
+                            </td>
+                        </tr>
+                    </table>
+
+                    <?if(is_array($arElement["DISPLAY_PROPERTIES"])):?>
+                        <div class="s7spb-product-props">
+                            <?foreach ($arElement["DISPLAY_PROPERTIES"] as $arProperty):?>
+                                <div class="pb1 text-11"><?=$arProperty["NAME"]?>: <b><?=$arProperty["DISPLAY_VALUE"]?></b></div>
+                            <?endforeach;?>
+                            <div class="pb1 text-11">Остаток: <b>>100</b></div>
+                        </div>
+                    <?endif;?>
+
+                    <div class="px5">
+                        <form active="<?=$arElement["DETAIL_PAGE_URL"]?>" method="post">
+
+                            <input type="hidden" name="action" value="ADD2BASKET">
+                            <input type="hidden" name="id" value="<?=$arElement["ID"]?>">
+                            <?=bitrix_sessid_post()?>
+
+                            <div class="s7spb-row quantity-counter">
+                                <div class="s7spb-col quantity-counter-minus"
+                                     onclick="quantityCounter.down(this)">&ndash;</div>
+                                <input type="text"
+                                       name="<?=$arParams["PRODUCT_QUANTITY_VARIABLE"]?>"
+                                       value="1"
+                                       class="s7spb-col" />
+                                <div class="s7spb-col quantity-counter-plus"
+                                     onclick="quantityCounter.up(this)">+</div>
+                            </div>
+                            <input type="submit" value=" " />
+                        </form>
                     </div>
-                <?endforeach;?>
+                <?endif;?>
+
+
+
+
             </div>
         </div>
     <?
